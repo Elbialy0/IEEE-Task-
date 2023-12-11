d7834716c06f8796a5ecce5f53bc1137293d45a4
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2023-12-11 20:47:45 +0200

    codeforces problem
     done

---
+++ codeforcesProblem.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class codeforcesProblem {
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        in.nextLine();
+        while(t-->0)
+        {
+            String str =in.next();
+            in.nextLine();
+            char [] arr = str.toCharArray();
+            boolean flag = false;
+            if(arr[0]=='a'&&arr[1]=='b'&&arr[2]=='c')
+            {
+                flag=true;
+            }
+            else if((arr[0]=='c'&&arr[2]=='a')||(arr[0]=='b'&&arr[1]=='a')||(arr[1]=='c'&&arr[2]=='b'))
+            {
+                flag=true;
+            }
+            if(flag) System.out.println("Yes");
+            else System.out.println("No");
+        }
+
+
+    }
+}
+
